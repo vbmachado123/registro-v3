4800b26c586481073381ade59fa7d3e54f570804
Victor <vbmachado123@gmail.com>
2020-11-27 19:01:42 -0300

    configuracoes iniciais

---
+++ app/src/main/java/br/com/registro/telas/HomeActivity.java
@@ -0,0 +1,16 @@
+package br.com.registro.telas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.registro.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/br/com/registro/telas/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.registro.telas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.registro.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
