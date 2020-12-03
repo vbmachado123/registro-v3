8d5255d95d99321c6643dc5c68283fb51abc0251
Victor Balbino <vbmachado123>
2020-12-03 13:34:03 -0300

    Criando interface da HOME

--- app/src/main/java/br/com/registro/telas/HomeActivity.java
+++ app/src/main/java/br/com/registro/telas/HomeActivity.java
@@ -3,14 +3,26 @@ package br.com.registro.telas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import br.com.registro.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private BottomNavigationView bnv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        bnv = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bnv.setBackground(null);
+
     }
 }
